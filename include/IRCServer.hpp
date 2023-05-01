9fc5cea63305819ffa5a5c9dd571c935a58c210f
davifah <davifarhi@gmail.com>
2023-05-01 11:06:31 +0200

    socket: open bind listen and close

--- include/IRCServer.hpp
+++ include/IRCServer.hpp
@@ -4,6 +4,11 @@
 
 #include <string>
 #include <iostream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <cerrno>
+#include <cstdio>
+#include <cstring>
 using std::cout;
 using std::cerr;
 using std::endl;
@@ -15,10 +20,15 @@ class IRCServer
 		const int port;
 		const string pswd;
 		bool has_started;
+		int sockfd;
 	public:
 		IRCServer( int port, string pswd );
+		~IRCServer( void );
 
 		bool get_has_started( void ) const;
+
+	private:
+		bool create_socket( void );
 };
 
 #endif /* end of include guard: IRCSERVER_H */
--- src/IRCServer.cpp
+++ src/IRCServer.cpp
@@ -12,11 +12,52 @@ IRCServer::IRCServer( int port, string pswd ) : port(port), pswd(pswd), has_star
 		cerr << "Invalid password " << pswd << ", it must have at least 4 characters\n";
 		return;
 	}
+	if (create_socket())
+		return;
 	cout << "Starting IRC server at port " << port << " with password: " << pswd << endl;
 	this->has_started = true;
+}
+
+IRCServer::~IRCServer( void )
+{
+	shutdown( sockfd, SHUT_RDWR );
 }
 
 bool IRCServer::get_has_started( void ) const
 {
 	return this->has_started;
 }
+
+bool IRCServer::create_socket( void )
+{
+	sockfd = socket( AF_INET, SOCK_STREAM, 0 );
+	if (sockfd == -1)
+	{
+		cerr << "IRCServer::create_socket: socket() error\n";
+		return true;
+	}
+
+	//if needed setsockopt here
+
+	{
+		struct sockaddr_in addr;
+		addr.sin_family = AF_INET;
+		addr.sin_addr.s_addr = INADDR_ANY;
+		addr.sin_port = htons(this->port);
+
+		if (bind( sockfd, (struct sockaddr*) &addr, sizeof(addr) ) == -1)
+		{
+			cerr << "IRCServer::create_socket: bind() error: " << strerror(errno) << endl;
+			return true;
+		}
+	}
+
+	if (listen( sockfd, 128 ) == -1)
+	{
+		cerr << "IRCServer::create_socket: listen() error: " << strerror(errno) << endl;
+		return true;
+	}
+
+	return false;
+}
+
